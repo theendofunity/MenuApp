cb3b4dbb0ec76f72919ed7a4666507696806e8f5
Dmitriy Dudkin <d.dudkin@growapp.biz>
2023-04-17 15:05:30 +0400

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Дмитрий Дудкин on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
